b55c7deb7ac57c8a71d79dd23f67ff751168b052
João Pedro <79864997+joaogomes00@users.noreply.github.com>
2022-12-05 21:27:20 -0300

    Add files via upload

---
+++ src/calculadora.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+class calculadora {
+
+	
+	public double somar(double num1, double num2){
+		return num1+num2;
+	}
+	public double subtrair(double num1, double num2){
+		return num1-num2;
+	}
+	
+	public double multiplicar(double num1, double num2){
+		return num1*num2;
+	}
+	public double dividir(double num1, double num2){
+		return num1/num2;
+	}
+}
